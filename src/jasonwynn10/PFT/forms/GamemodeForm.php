0ea0eb4ac7110710ebb1f89abd56a9d47dc15318
jasonwynn10 <jasonwynn10@gmail.com>
2018-01-07 22:33:12 -0500

    Add base files

---
+++ src/jasonwynn10/PFT/forms/GamemodeForm.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+namespace jasonwynn10\PFT\forms;
+
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\form\Form;
+use pocketmine\form\MenuForm;
+use pocketmine\form\MenuOption;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class GamemodeForm extends MenuForm {
+	/**
+	 * GamemodeForm constructor.
+	 */
+	public function __construct() {
+		$options = [];
+		$options[] = new MenuOption("Survival");
+		$options[] = new MenuOption("Creative");
+		$options[] = new MenuOption("Adventure");
+		$options[] = new MenuOption("Spectator");
+		parent::__construct("Gamemode Selector", "Choose a gamemode from the list", $options);
+	}
+
+	/**
+	 * @param Player $player
+	 *
+	 * @return null|Form
+	 */
+	public function onSubmit(Player $player) : ?Form {
+		Server::getInstance()->dispatchCommand(new ConsoleCommandSender(), "gamemode ".$this->getSelectedOption()->getText()." ".$player->getName());
+		return null;
+	}
+}
